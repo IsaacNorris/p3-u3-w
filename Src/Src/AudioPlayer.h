1f2b89143a6456abc738facafc95f597c5b42bfc
Isaac Norris <isaac.a.norris@gmail.com>
2026-08-09 02:35:18 +1000

    added in basic outline to the audio player class.

--- Src/Src/AudioPlayer.h
+++ Src/Src/AudioPlayer.h
@@ -8,7 +8,18 @@ class tAudioPlayer {
    public:
     tAudioPlayer();
 
+    void PlayPause();
+    void IncrementVolume();
+    void DecrementVolume();
+    void NextSong();
+    void PreviousSong();
    private:
 };
 
 inline tAudioPlayer::tAudioPlayer() {}
+
+inline void tAudioPlayer::PlayPause() {}
+inline void tAudioPlayer::IncrementVolume() {}
+inline void tAudioPlayer::DecrementVolume() {}
+inline void tAudioPlayer::NextSong() {}
+inline void tAudioPlayer::PreviousSong() {}
