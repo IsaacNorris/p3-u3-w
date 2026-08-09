a0f206e6c307f0e5a302b2dfb2f2d25512a45375
Isaac Norris <isaac.a.norris@gmail.com>
2026-08-10 03:06:05 +1000

    added in some logic for handling song and playlist selection at the DSA level.

--- Src/Src/MusicManager.h
+++ Src/Src/MusicManager.h
@@ -11,6 +11,10 @@ class tMusicManager {
     tMusicManager();
 
     void Update();
+
+    tPlaylist* CurrentPlaylist();
+    tSong* CurrentSong();
+
     void IncrementVolume();
     void DecrementVolume();
     void NextSong();
@@ -25,19 +29,64 @@ class tMusicManager {
     tAudioPlayer audioPlayer_;
     std::vector<tPlaylist*> playlists_;
 
+    std::vector<tPlaylist*>::iterator currentPlaylistItr_ {playlists_.end()};
+
     tVolume volume_;
     tVolumeValue volumeStep_{5};
+
+    bool isPlaying_ {false};
 
     void BuildPlaylists();
 };
 
-inline tMusicManager::tMusicManager() {}
+inline tMusicManager::tMusicManager() {
+    // currentPlaylistItr_ = playlists_.end();
+}
 
-inline void tMusicManager::Update() {}
+inline void tMusicManager::Update() {
+    //where we will put the actual interface to the audio
+    // i.e. the WRITE
+}
+
+inline tPlaylist* tMusicManager::CurrentPlaylist(){
+    return *currentPlaylistItr_;
+}
+
+inline tSong* tMusicManager::CurrentSong(){
+    tPlaylist* playlist = *currentPlaylistItr_;
+    return playlist->CurrentSong();
+}
 
 inline void tMusicManager::IncrementVolume() { volume_ + volumeStep_; }
 
 inline void tMusicManager::DecrementVolume() { volume_ - volumeStep_; }
+
+inline void tMusicManager::NextSong(){
+    tPlaylist* playlist = *currentPlaylistItr_;
+    playlist->NextSong();
+}
+
+inline void tMusicManager::PreviousSong(){
+    tPlaylist* playlist = *currentPlaylistItr_;
+    playlist->PreviousSong();
+}
+
+inline void tMusicManager::PausePlaySong() { isPlaying_ = !isPlaying_; }
+
+inline void tMusicManager::NextPlaylist(){
+    if(currentPlaylistItr_ == playlists_.end()){
+        Log::Error("Current Playlist Iterator invalid");
+        return;
+    }
+
+    ++currentPlaylistItr_;
+    if(currentPlaylistItr_ == playlists_.end()){
+        currentPlaylistItr_ = playlists_.begin();
+    }
+
+    tPlaylist* playlist = *currentPlaylistItr_;
+    playlist->LoadPlaylist();
+}
 
 inline void tMusicManager::SetVolumeStep(tVolumeValue step) {
     if (step >= 100) {
@@ -48,11 +97,16 @@ inline void tMusicManager::SetVolumeStep(tVolumeValue step) {
 }
 
 // we gotta be careful with the dynamic allocation.
+// this function should only ever be called on startup.
 inline void tMusicManager::BuildPlaylists(){
     playlists_.clear();
+    currentPlaylistItr_ = playlists_.end();
+
     auto folders = fileManager.GetFolders();
     for(auto folder : folders){
         tPlaylist* p = new tPlaylist(folder);
         playlists_.push_back(p);
     }
+
+    currentPlaylistItr_ = playlists_.begin();
 }
--- Src/Src/Song.h
+++ Src/Src/Song.h
@@ -26,36 +26,84 @@ class tPlaylist {
    public:
     tPlaylist(tFolder* folder);
 
+    tSong* CurrentSong();
+
     const char* PlaylistName();
 
     void LoadPlaylist();
     void UnloadPlaylist();
 
+    void NextSong();
+    void PreviousSong();
+
    private:
     std::vector<tSong*> songs_;
+    std::vector<tSong*>::iterator currentSongItr_ {songs_.end()};
+
     tFolder* folder_;
 
     bool playlistLoaded_{false};
 };
 
 inline tPlaylist::tPlaylist(tFolder* folder) : folder_(folder) {}
+
+inline tSong* tPlaylist::CurrentSong() {
+    return *currentSongItr_;
+}
+
 inline const char* tPlaylist::PlaylistName() {
     return folder_->GetFolderName();
+}
+
+inline void tPlaylist::NextSong() {
+    if(currentSongItr_ == songs_.end()){
+        Log::Error("Current Song Iterator invalid");
+        return;
+    }
+
+    ++currentSongItr_;
+
+    if(currentSongItr_ == songs_.end()){
+        currentSongItr_ = songs_.begin();
+    }
+}
+
+inline void tPlaylist::PreviousSong() {
+    if(currentSongItr_ == songs_.end()){
+        Log::Error("Current Song Iterator invalid");
+        return;
+    }
+
+    if(currentSongItr_ == songs_.begin()){
+        currentSongItr_ = songs_.end();
+    }
+
+    --currentSongItr_;
 }
 
 inline void tPlaylist::LoadPlaylist() {
     bool success = false;
     // based on all the mp3 files under the folder_.FullPath
     // build up a vector of songs.
+
     songs_.clear();
+    currentSongItr_ = songs_.end();
     std::vector<tFile*> files = fileManager.GetFiles(folder_->GetFullPath());
+
     for (tFile* file : files) {
         songs_.push_back(new tSong(file));
     }
-    playlistLoaded_ = true;
+
+    //TODO: find a better way of tracking success;
+    success = true;
+
+    playlistLoaded_ = success;
+
+    currentSongItr_ = songs_.begin();
 }
 
 inline void tPlaylist::UnloadPlaylist() {
     songs_.clear();
+    currentSongItr_ = songs_.end();
     playlistLoaded_ = false;
 }
